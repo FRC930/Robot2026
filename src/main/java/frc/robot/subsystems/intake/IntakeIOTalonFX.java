03b977343c72b2e18a4a342da1b5b3f6804e754e
EphraimSonnenberg <ephraimsonnenberg@gmail.com>
2026-02-09 20:20:27 -0600

    Fixed #40 Github issue

--- src/main/java/frc/robot/subsystems/intake/IntakeIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOTalonFX.java
@@ -77,9 +77,10 @@ public class IntakeIOTalonFX implements IntakeIO {
 
   @Override
   public void updateInputs(IntakeInputs inputs) {
-    inputs.intakeVoltage.mut_replace(followIntakeMotor.getMotorVoltage().getValue());
+    inputs.intakeVoltage.mut_replace(leaderIntakeMotor.getMotorVoltage().getValue());
     inputs.intakeSetVoltage.mut_replace(intakeSetPoint);
-    inputs.intakeSupplyCurrent.mut_replace(intakeExtenderMotor.getSupplyCurrent().getValue());
+    inputs.intakeSupplyCurrent.mut_replace(leaderIntakeMotor.getSupplyCurrent().getValue());
+    
     inputs.intakeExtenderVoltage.mut_replace(intakeExtenderMotor.getMotorVoltage().getValue());
     inputs.intakeExtenderSetVoltage.mut_replace(intakeExtenderSetPoint);
     inputs.intakeExtenderSupplyCurrent.mut_replace(
