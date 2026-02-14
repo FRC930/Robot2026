671c66ffbe482630162b7da4531fa2ade662190f
Luke Wroblewski <wrobluke@stu.masd.k12.wi.us>
2026-02-14 10:32:05 -0600

    Check intake / outtake logic

--- src/main/java/frc/robot/goals/RobotGoals.java
+++ src/main/java/frc/robot/goals/RobotGoals.java
@@ -19,7 +19,7 @@ public class RobotGoals extends VirtualSubsystem implements RobotEvents {
   private static RobotGoals instance;
 
   private final EnumState<RobotGoal> currentGoal =
-      new EnumState<>("RobotGoals/Goal", RobotGoal.IDLE);
+      new EnumState<>("RobotGoals/Goal", RobotGoal.INTAKING);
 
   private RobotGoals() {}
 
