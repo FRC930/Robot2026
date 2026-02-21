526072a6b7c7b2cd463b543c0e85a8cf5e2691fd
avmhh <hutchisonava14@gmail.com>
2026-02-21 14:12:48 -0600

    Disabled testing mode

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -112,7 +112,7 @@ public class RobotContainer {
 
   // Set to true when Testing Individual subsystems
   // This should stay false otherwise
-  private static final boolean ISTESTING = true;
+  private static final boolean ISTESTING = false;
 
   private final AprilTagVision vision;
 
