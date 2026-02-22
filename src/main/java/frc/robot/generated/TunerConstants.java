d56b5b5bfa9746c448e66424e56617782fef2c8a
austinL28 <austinledin28@icloud.com>
2026-02-22 08:37:35 -0600

    fix tunable numbers for indexer and feeder

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -32,7 +32,13 @@ public class TunerConstants {
   // When using closed-loop control, the drive motor uses the control
   // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
   private static final Slot0Configs driveGains =
-      new Slot0Configs().withKP(0.28381775).withKI(0).withKD(0).withKS(0.34163).withKV(0.81075).withKA(0.0629435);
+      new Slot0Configs()
+          .withKP(0.28381775)
+          .withKI(0)
+          .withKD(0)
+          .withKS(0.34163)
+          .withKV(0.81075)
+          .withKA(0.0629435);
 
   // The closed-loop output type to use for the steer motors;
   // This affects the PID/FF gains for the steer motors
--- src/main/java/frc/robot/subsystems/indexer/IndexerState.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerState.java
@@ -2,29 +2,32 @@ package frc.robot.subsystems.indexer;
 
 import static edu.wpi.first.units.Units.RPM;
 
+import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
+
 import edu.wpi.first.units.measure.AngularVelocity;
 import frc.robot.util.LoggedTunableNumber;
 
 public enum IndexerState {
-  TESTING(RPM.zero(), RPM.zero()),
-  IDLE(RPM.zero(), RPM.zero()),
+  TESTING(() -> 0.0, () -> 0.0),
+  IDLE(() -> 0.0 , () ->  0.0),
   FEEDING( // TODO get log tunable numbers to work
-      RPM.of(new LoggedTunableNumber("Indexer/setIndexerPoint", 100).get()),
-      RPM.of(new LoggedTunableNumber("Indexer/setFeederPoint", 1000).get()));
+      new LoggedTunableNumber("Indexer/setIndexerPoint", 100),
+      new LoggedTunableNumber("Indexer/setFeederPoint", 1000));
 
-  private AngularVelocity m_indexerVelocity;
-  private AngularVelocity m_feederVelocity;
+  private DoubleSupplier m_indexerVelocity;
+  private DoubleSupplier m_feederVelocity;
 
-  private IndexerState(AngularVelocity indexerVelocity, AngularVelocity feederVelocity) {
+  private IndexerState(DoubleSupplier indexerVelocity, DoubleSupplier feederVelocity) {
     m_indexerVelocity = indexerVelocity;
     m_feederVelocity = feederVelocity;
   }
 
   public AngularVelocity indexerVelocity() {
-    return m_indexerVelocity;
+    return  RPM.of(m_indexerVelocity.getAsDouble());
   }
 
   public AngularVelocity feederVelocity() {
-    return m_feederVelocity;
+    return RPM.of(m_feederVelocity.getAsDouble());
   }
 }
