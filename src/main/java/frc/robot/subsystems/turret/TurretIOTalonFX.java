341e2fcb99e0cb10c3f89c85cd67e320c1652487
avmhh <hutchisonava14@gmail.com>
2026-02-28 09:56:16 -0600

    created default cancoder configuration

--- src/main/java/frc/robot/subsystems/turret/TurretIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/turret/TurretIOTalonFX.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.turret;
 import static edu.wpi.first.units.Units.Degrees;
 
 import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.PositionVoltage;
@@ -11,6 +12,8 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.GravityTypeValue;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
+
 import edu.wpi.first.units.measure.Angle;
 import frc.robot.aiming.AimingConstants;
 import frc.robot.util.Gains;
@@ -104,7 +107,6 @@ public class TurretIOTalonFX implements TurretIO {
   }
 
   public void configureTalons() {
-
     TalonFXConfiguration cfg = new TalonFXConfiguration();
     cfg.MotorOutput.NeutralMode = NeutralModeValue.Brake;
     cfg.CurrentLimits.SupplyCurrentLimit = 40.0;
@@ -126,6 +128,22 @@ public class TurretIOTalonFX implements TurretIO {
     motor.getPosition().setUpdateFrequency(AimingConstants.AIMING_FREQUENCY);
     motor.getVelocity().setUpdateFrequency(AimingConstants.AIMING_FREQUENCY);
     motor.optimizeBusUtilization();
+  }
+
+  public void configureCANCoders() {
+    // Configure CANcoder 1
+    PhoenixUtil.tryUntilOk(5, () -> canCoder1.getConfigurator().apply(new CANcoderConfiguration()));
+    CANcoderConfiguration cfg1 = new CANcoderConfiguration();
+    cfg1.MagnetSensor.SensorDirection = SensorDirectionValue.CounterClockwise_Positive;
+    cfg1.MagnetSensor.AbsoluteSensorDiscontinuityPoint = 1; // [0, 1] (0° or 360°) 
+    PhoenixUtil.tryUntilOk(5, () -> canCoder1.getConfigurator().apply(cfg1));
+
+    // Configure CANcoder 2
+    PhoenixUtil.tryUntilOk(5, () -> canCoder2.getConfigurator().apply(new CANcoderConfiguration()));
+    CANcoderConfiguration cfg2 = new CANcoderConfiguration();
+    cfg2.MagnetSensor.SensorDirection = SensorDirectionValue.CounterClockwise_Positive;
+    cfg2.MagnetSensor.AbsoluteSensorDiscontinuityPoint = 1; // [0, 1] (0° or 360°) 
+    PhoenixUtil.tryUntilOk(5, () -> canCoder2.getConfigurator().apply(cfg2));
   }
 
   @Override
