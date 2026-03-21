1238febc29cbb8d7b2e3111f1ce64dd22092d84b
chasereinders-dot <chase.reinders@gmail.com>
2026-03-21 15:31:32 -0500

    Formator

--- src/main/java/frc/robot/AutoCommandManager.java
+++ src/main/java/frc/robot/AutoCommandManager.java
@@ -7,7 +7,6 @@ import com.pathplanner.lib.util.FlippingUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.goals.RobotGoal;
 import frc.robot.goals.RobotGoals;
 import frc.robot.subsystems.drive.Drive;
