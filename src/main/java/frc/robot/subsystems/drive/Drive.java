0ce5635892fef6e5725cd89986ba2c20dc7c8fae
austinL28 <austinledin28@icloud.com>
2026-04-11 17:50:30 -0500

    changes to drive frequency to reduce loop overrun

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -65,7 +65,7 @@ import org.littletonrobotics.junction.Logger;
 
 public class Drive extends SubsystemBase {
   // TunerConstants doesn't include these constants, so they are declared locally
-  static final double ODOMETRY_FREQUENCY = TunerConstants.kCANBus.isNetworkFD() ? 250.0 : 100.0;
+  static final double ODOMETRY_FREQUENCY = TunerConstants.kCANBus.isNetworkFD() ? 125.0 : 50.0;
   public static final double DRIVE_BASE_RADIUS =
       Math.max(
           Math.max(
