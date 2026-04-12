191e62ccb716c635653667c290a074e0399ac621
Austin L <austinledin28@icloud.com>
2026-04-11 20:53:22 -0500

    new gear ratio YAA! with 3d printed gears

--- src/main/java/frc/robot/subsystems/extender/ExtenderSubsystem.java
+++ src/main/java/frc/robot/subsystems/extender/ExtenderSubsystem.java
@@ -31,7 +31,7 @@ public class ExtenderSubsystem extends SubsystemBase implements ExtenderEvents {
 
   public static final double SPOOL_RADIUS = INCHES_PER_ROT / (2.0 * Math.PI);
 
-  public static final double REDUCTION = (2.0 / 3.0);
+  public static final double REDUCTION = (50.0 / 15.0);
 
   // Agitation state
   private TrapezoidProfile agitateProfile;
