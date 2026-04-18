4fada4a8e788610c9a4499fbfbd5cb96633ea4c8
austinL28 <austinledin28@icloud.com>
2026-04-18 16:53:31 -0500

    Dont zero intake (in case reboot)

--- src/main/java/frc/robot/subsystems/extender/ExtenderIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/extender/ExtenderIOTalonFX.java
@@ -104,11 +104,11 @@ public class ExtenderIOTalonFX implements ExtenderIO {
 
     PhoenixUtil.tryUntilOk(5, () -> followerMotor.getConfigurator().apply(cfgFollower));
 
-    followerMotor.setPosition(0.0);
+    // followerMotor.setPosition(0.0);
     followerMotor.setControl(
         new DifferentialFollower(extenderMotor.getDeviceID(), MotorAlignmentValue.Opposed));
 
-    extenderMotor.setPosition(0.0);
+    // extenderMotor.setPosition(0.0);
   }
 
   @Override
